bfea1b3bd6f850c788e2bddbf14e68a965081f1d
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-23 11:32:53 +0600

    Create Adjacency_matrix.cpp

---
+++ Graph/Adjacency_matrix.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int vertex,edge;
+    cout<<"Enter the number of vertex: ";
+    cin>>vertex;
+    cout<<endl;
+    cout<<"Enter the number of edges: ";
+    cin>>edge;
+
+    int adjacencymatrix[vertex][vertex];
+    for(int i = 0; i<vertex; i++)
+    {
+        for(int j = 0; j<vertex; j++)
+        {
+            adjacencymatrix[i][j]=0;
+        }
+    }
+   
+   // Enter the edges for undirected graph
+   for(int i = 0; i<edge; i++)
+   {
+    int u, v;
+    cin>>u,v;
+    if(u>vertex || v>vertex)
+    {
+        cout<<"invalid edge"<<endl;
+        i--;
+    }
+    else{
+        adjacencymatrix[u][v] = 1;
+        adjacencymatrix[v][u] = 1; //skip this for directed graph;
+    }
+   }
+
+   for(int i = 0; i<vertex; i++)
+   {
+     for(int j = 0; j<vertex; j++)
+     {
+       cout<<adjacencymatrix[i][j]<<"";
+     }
+     cout<<endl;
+   }
+    return 0;
+}
